f2ab044b1205d914c271e5f88db72ec80d4b00ac
Daniel Guzmán <danieljk001@yahoo.com>
2023-06-09 09:55:59 -0500

    Creación del navbar, estilos y responsivo

---
+++ proyecto_final/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import style from '../styles/navbar.module.css'
+
+const Navbar = () => {
+
+    let [show,setShow] = useState(false);
+
+    return(<>
+
+            <header id={style.cabeza}>
+
+            <picture id={style.picture_navbar}>
+                <img src="./logo.png" alt="Logo de la empresa" />
+            </picture>
+
+            <nav id={style.navbar}>
+                <a className={style.btn_navbar} href="#" title='Apartado de Dashboard'>Dashboard</a>
+                <a className={style.btn_navbar} href="#" title='Apartado de productos'>Productos</a>
+                <a className={style.btn_navbar} href="#" title='Apartado de pedidos'>Pedidos</a>
+            </nav>
+
+
+        </header>
+        
+    </>);
+}
+
+export default Navbar;
