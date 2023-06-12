b4489ef7d847d86b58f8db0d86b3fb37b30373da
Daniel Guzmán <danieljk001@yahoo.com>
2023-06-12 09:34:18 -0500

    Creación de la pantalla de carga

--- proyecto_final/src/components/Dashboard.jsx
+++ proyecto_final/src/components/Dashboard.jsx
@@ -4,16 +4,37 @@ import Promedio from "./Promedio";
 import TotalPedidos from "./TotalPedidos";
 import Ingresos from "./Ingresos";
 import Top from "./Top";
+import style from '../styles/dashboard.module.css'
+import ProductContext from "../context/ProductContext";
+import OrderContext from "../context/OrderContext";
+import { useContext } from "react";
 
 const Dashboard = () => {
 
+    const { productos, listName, estadoProducto  } = useContext(ProductContext);
+    const { orders } = useContext(OrderContext);
+
     return(<>
-        <Navbar />
-        <TotalProducto />
-        <Promedio />
-        <TotalPedidos />
-        <Ingresos />
-        <Top />
+
+        {( productos && listName && estadoProducto && orders ) ? (
+
+            <section>
+                <Navbar />
+                <TotalProducto />
+                <Promedio />
+                <TotalPedidos />
+                <Ingresos />
+                <Top />
+            </section>
+
+        ) : (
+
+            <section id={style.preloader_3}>
+            </section>
+
+        )}
+
+
     </>);
 }
 
